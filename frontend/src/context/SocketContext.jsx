e3d1d1a2846c09891c9003ea394d0547121cae95
anmolsinghrawat <rawatanmol88@gmail.com>
2025-10-25 18:09:45 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://pgen-post.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
